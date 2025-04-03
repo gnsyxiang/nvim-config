3fb9c0642a5830e4c0504e20c965a61d376d9279
zhenquan.qiu <gnsyxiang@163.com>
2025-04-03 13:58:15 +0800

    feat: update keymaps.lua

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -5,14 +5,24 @@ local opts = {
 }
 local map = vim.keymap.set
 
-map("i", "jk", "<ESC>", opts)
+map("i", "jk",			"<ESC>",		opts)
 
 -- alt + hjkl  窗口之间跳转
-map("n", "<A-h>", "<C-w>h", opts)
-map("n", "<A-j>", "<C-w>j", opts)
-map("n", "<A-k>", "<C-w>k", opts)
-map("n", "<A-l>", "<C-w>l", opts)
+map("n", "<A-h>",		"<C-w>h",		opts)
+map("n", "<A-j>", 		"<C-w>j", 		opts)
+map("n", "<A-k>", 		"<C-w>k", 		opts)
+map("n", "<A-l>", 		"<C-w>l", 		opts)
 
+-- 多窗口的打开与关闭
+map("n", "s",			"",				opts)    -- 取消 s 默认功能
+map("n", "sv", 			":vsp<CR>",  	opts)
+map("n", "sh", 			":sp<CR>",   	opts)
+map("n", "sc", 			"<C-w>c",    	opts)
+map("n", "so", 			"<C-w>o",    	opts)
+
+-- 退出
+map("n", "qq",			":q!<CR>",      opts)
+map("n", "<leader>q",	":qa!<CR>",     opts)
 
 vim.cmd([[
     nnoremap ; :
@@ -23,4 +33,10 @@ vim.cmd([[
     :command Qa qa
     :command QA qa
 ]])
+
+vim.cmd([[
+	set clipboard+=unnamed,unnamedplus
+	" set clipboard+=unnamedplus
+]])
+
 
