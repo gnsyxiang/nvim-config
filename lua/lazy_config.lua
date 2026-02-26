0451dd0e4d579a70493d616acd779beb4c5aa67f
zhenquan.qiu <gnsyxiang@163.com>
2026-02-26 09:11:29 +0800

    feat: add lazy_config.lua

---
+++ lua/lazy_config.lua
@@ -0,0 +1,44 @@
+
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "git@github.com:folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  {
+    "nvim-tree/nvim-tree.lua",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      require("plugins.nvim-tree")
+    end,
+    lazy = false,
+  },
+}, {
+  ui = {
+    border = "rounded",
+  },
+  performance = {
+    rtp = {
+      disabled_plugins = { "netrw", "netrwPlugin" },
+    },
+  },
+  git = {
+    url_format = "git@github.com:%s.git",
+    timeout = 300,
+  },
+})
+
