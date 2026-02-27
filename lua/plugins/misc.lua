9fc8b87badef089f937ae40ce925e20504fe842f
zhenquan.qiu <gnsyxiang@163.com>
2026-02-27 11:28:06 +0800

    feat: add nvim-surround.lua

---
+++ lua/plugins/misc.lua
@@ -0,0 +1,12 @@
+
+return {
+    {
+        "kylechui/nvim-surround",
+        version = "^4.0.0",
+        event = "VeryLazy",
+        config = function()
+            require("configs.nvim-surround")
+        end
+    },
+}
+
