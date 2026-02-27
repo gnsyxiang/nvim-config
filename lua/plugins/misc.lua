069783c512eab474988a4b37abba8faac90dd8db
zhenquan.qiu <gnsyxiang@163.com>
2026-02-27 15:06:42 +0800

    feat: add which-key.lua

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -8,5 +8,16 @@ return {
             require("configs.nvim-surround")
         end
     },
+    {
+        "folke/which-key.nvim",
+        event = "VeryLazy",
+        init = function()
+            vim.o.timeout = true
+            vim.o.timeoutlen = 300
+        end,
+        config = function()
+            require("configs.which-key")
+        end,
+    },
 }
 
