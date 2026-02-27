42bd1aa4e674c1f22bfe5fd9c24ca74fd8537d97
zhenquan.qiu <gnsyxiang@163.com>
2026-02-27 08:54:15 +0800

    refactor: refactor keymaps.lua

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,42 +1,42 @@
 
-local function map(mode, lhs, rhs, opts)
+local function map(mode, lhs, rhs, desc)
     local options = {noremap = true, silent = true}
 
-    if opts then
-        options = vim.tbl_extend("force", options, opts)
+    if desc then
+        options = vim.tbl_extend("force", options, {desc = 'keymaps: ' .. desc})
     end
 
     vim.keymap.set(mode, lhs, rhs, options)
 end
 
-map("i", "jk",          "<ESC>",        {desc = ""})
+map("i", "jk",          "<ESC>",        "")
 
-map("n", "qq",          ":q!<CR>",      {desc = ""})
-map("n", "<leader>q",   ":qa!<CR>",     {desc = ""})
+map("n", "qq",          ":q!<CR>",      "")
+map("n", "<leader>q",   ":qa!<CR>",     "")
 
 -- 多窗口的打开与关闭
-map("n", "s",           "",             {desc = ""})    -- 取消 s 默认功能
-map("n", "sv",          ":vsp<CR>",     {desc = ""})
-map("n", "sh",          ":sp<CR>",      {desc = ""})
-map("n", "sc",          "<C-w>c",       {desc = ""})
-map("n", "so",          "<C-w>o",       {desc = ""})
+map("n", "s",           "",             "")    -- 取消 s 默认功能
+map("n", "sv",          ":vsp<CR>",     "")
+map("n", "sh",          ":sp<CR>",      "")
+map("n", "sc",          "<C-w>c",       "")
+map("n", "so",          "<C-w>o",       "")
 
 -- alt + hjkl  窗口之间跳转
-map("n", "<A-h>",       "<C-w>h",       {desc = ""})
-map("n", "<A-j>",       "<C-w>j",       {desc = ""})
-map("n", "<A-k>",       "<C-w>k",       {desc = ""})
-map("n", "<A-l>",       "<C-w>l",       {desc = ""})
+map("n", "<A-h>",       "<C-w>h",       "")
+map("n", "<A-j>",       "<C-w>j",       "")
+map("n", "<A-k>",       "<C-w>k",       "")
+map("n", "<A-l>",       "<C-w>l",       "")
 
 -- 左右比例控制
-map("n", "s,",          ":vertical resize -10<CR>", {desc = ""})
-map("n", "s.",          ":vertical resize +10<CR>", {desc = ""})
-map("n", "<C-Left>",    ":vertical resize -2<CR>",  {desc = ""})
-map("n", "<C-Right>",   ":vertical resize +2<CR>",  {desc = ""})
+map("n", "s,",          ":vertical resize -10<CR>", "")
+map("n", "s.",          ":vertical resize +10<CR>", "")
+map("n", "<C-Left>",    ":vertical resize -2<CR>",  "")
+map("n", "<C-Right>",   ":vertical resize +2<CR>",  "")
 -- 上下比例
-map("n", "sj",          ":resize +10<CR>",          {desc = ""})
-map("n", "sk",          ":resize -10<CR>",          {desc = ""})
-map("n", "<C-Down>",    ":resize +2<CR>",           {desc = ""})
-map("n", "<C-Up>",      ":resize -2<CR>",           {desc = ""})
+map("n", "sj",          ":resize +10<CR>",          "")
+map("n", "sk",          ":resize -10<CR>",          "")
+map("n", "<C-Down>",    ":resize +2<CR>",           "")
+map("n", "<C-Up>",      ":resize -2<CR>",           "")
 -- 相等比例
 map("n", "s=", "<C-w>=", opts)
 
