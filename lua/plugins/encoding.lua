2e6e0dd3535cfb95c499e764da4031458ed4abe7
zhenquan.qiu <gnsyxiang@163.com>
2026-04-29 10:33:36 +0800

    feat: add neogen.lua

--- lua/plugins/encoding.lua
+++ lua/plugins/encoding.lua
@@ -7,5 +7,16 @@ return {
         -- use opts = {} for passing setup options
         -- this is equivalent to setup({}) function
     },
+    {
+        "danymat/neogen",
+        dependencies = "nvim-treesitter/nvim-treesitter", -- 依赖语法解析
+        cmd = "Neogen", -- 命令触发懒加载
+        config = function()
+            require("configs.neogen")
+        end,
+        keys = {
+            { "<leader>nf", function() require("neogen").generate() end, desc = "生成函数/类注释" },
+        },
+    },
 }
 
