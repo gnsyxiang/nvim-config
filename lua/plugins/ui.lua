cba79850baaea6cc2a274bf9458fccaf13b5a003
zhenquan.qiu <gnsyxiang@163.com>
2026-08-25 17:08:35 +0800

    feat: add indent-blankline.lua

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -7,5 +7,13 @@ return {
             require("configs.gruvbox")
         end,
     },
+    {
+        "lukas-reineke/indent-blankline.nvim",
+        event = { "BufReadPost", "BufNewFile" },
+        main = "ibl",                               -- V3 版本的主模块名
+        config = function()
+            require("configs.indent-blankline")
+        end,
+    },
 }
 
