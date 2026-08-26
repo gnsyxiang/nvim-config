aec49930a65039fa50b6524e70a37936b85d9baa
zhenquan.qiu <gnsyxiang@163.com>
2026-08-26 15:05:01 +0800

    feat: add telescope.lua file

---
+++ lua/configs/telescope.lua
@@ -0,0 +1,183 @@
+
+local telescope = require("telescope")
+local actions = require("telescope.actions")
+local builtin = require("telescope.builtin")
+-- local open_with_trouble = require("trouble.sources.telescope").open
+
+local opts = {
+    defaults = {
+        path_display = { "smart" },         -- 智能缩短路径，平衡信息量和可读性
+        sorting_strategy = 'ascending',     -- 让匹配度最高的结果在顶部
+        layout_strategy = 'horizontal',     -- 或 'vertical'、'center' 等
+        layout_config = {
+            horizontal = {prompt_position = "top"},
+        },
+
+        prompt_prefix = "🔍 ",
+        selection_caret = " ",
+        file_ignore_patterns = {
+            ".git", ".cache",
+            "node_modules", "build", "target", "dist",
+            "%.o", "%.a", "%.out", "%.class",
+            "%.pdf", "%.mkv", "%.mp4", "%.zip",
+        },
+
+        -- 文件查找器（优先使用 fd）
+        find_command = {
+            "fd",
+            "--type",
+            "f",
+            "--hidden",
+            "--exclude",
+            ".git",
+        },
+
+        -- 实时 grep 命令（优先使用 ripgrep）
+        vimgrep_arguments = {
+            "rg",
+            "--color=never",
+            "--no-heading",
+            "--with-filename",
+            "--line-number",
+            "--column",
+            "--smart-case",
+            "--hidden",
+            "--glob=!.git/*",
+        },
+
+        mappings = {
+            i = {
+                ["<ESC>"]   = actions.close,
+
+                ["<A-j>"]   = actions.move_selection_next,
+                ["<A-k>"]   = actions.move_selection_previous,
+                ["<A-u>"]   = actions.preview_scrolling_up,
+                -- ["<A-d>"]   = actions.preview_scrolling_down,
+
+                ["<A-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+                -- ["<A-t>"] = open_with_trouble,
+
+                ["<A-d>"] = function(prompt_bufnr)
+                    local actions = require("telescope.actions")
+                    local action_state = require("telescope.actions.state")
+                    local selection = action_state.get_selected_entry()
+                    if selection then
+                        local commit_hash = selection.value                 -- 获取提交哈希（Telescope git_commits 的 entry.value 是哈希值）
+                        actions.close(prompt_bufnr)                         -- 关闭 Telescope 窗口
+                        vim.cmd("DiffviewOpen " .. commit_hash .. "^!")     -- 用 Diffview 打开该提交（加上 ^! 表示只显示该提交引入的变更）
+                    end
+                end,
+
+                ["<CR>"]    = actions.select_default,
+                ["<A-h>"]   = actions.select_horizontal,
+                ["<A-v>"]   = actions.select_vertical,
+                ["<C-t>"]   = actions.select_tab,
+            },
+            n = {
+                ["q"]       = actions.close,
+
+                -- ["<CR>"] = actions.select_default,
+                ["o"]       = actions.select_default,
+                ["H"]       = actions.select_horizontal,
+                ["V"]       = actions.select_vertical,
+                ["T"]       = actions.select_tab,
+
+                ["j"]       = actions.move_selection_next,
+                ["k"]       = actions.move_selection_previous,
+                ["K"]       = actions.preview_scrolling_up,
+                ["J"]       = actions.preview_scrolling_down,
+                -- ["H"]       = actions.move_to_top,
+                ["gg"]      = actions.move_to_top,
+                ["M"]       = actions.move_to_middle,
+                --["L"]       = actions.move_to_bottom,
+                ["G"]       = actions.move_to_bottom,
+            },
+        },
+        set_env = { ["COLORTERM"] = "truecolor" }, -- 启用真彩色
+    },
+    pickers = {
+        -- 针对特定选择器的定制
+        -- find_files = {
+        --     hidden = true, -- 显示隐藏文件
+        --     find_command = { "fdfind", "--type", "f", "--strip-cwd-prefix" }, -- 如果安装了 fd，优先使用
+        -- },
+        -- find_files = {
+        --     hidden = true,
+        --     -- 自动检测 fd，如果命令名不是 fd 请修改
+        --     find_command = vim.fn.executable('fd') == 1 and { 'fd', '--type', 'f', '--strip-cwd-prefix' }
+        --         or (vim.fn.executable('find') == 1 and { 'find', '--type', 'f', '--strip-cwd-prefix' })
+        --         or nil, -- 回退到默认 find
+        -- },
+        live_grep = {
+            additional_args = { "--hidden" }, -- 搜索隐藏文件
+        },
+    },
+    extensions = {
+        fzf = {
+            fuzzy = true,                       -- 启用模糊匹配
+            override_generic_sorter = true,     -- 覆盖通用排序器
+            override_file_sorter = true,        -- 覆盖文件排序器
+            case_mode = "smart_case",           -- 智能大小写
+        },
+        ["ui-select"] = {
+            require("telescope.themes").get_dropdown {
+                -- 这里可以定制下拉菜单的主题，例如：
+                -- theme = "dropdown",
+                -- winblend = 10,
+                -- border = true,
+            }
+        },
+        live_grep_args = {
+            auto_quoting = true, -- 自动将未加引号的词视为一个整体，如搜索 "foo bar"[reference:10][reference:11]
+        },
+        lazy = {
+            icon = "", -- 第一列显示的图标
+            -- theme = "ivy", -- 可选主题
+            show_icon = true, -- 是否显示图标
+            mappings = { -- 自定义按键映射
+                open_in_browser = "<C-o>",
+                open_in_file_browser = "<M-b>",
+                open_in_find_files = "<C-f>",
+                open_in_live_grep = "<C-g>",
+                open_in_terminal = "<C-t>",
+                open_plugins_picker = "<C-b>",
+                open_lazy_root_find_files = "<C-r>f",
+                open_lazy_root_live_grep = "<C-r>g",
+                change_cwd_to_plugin = "<C-c>d",
+            },
+        },
+    },
+}
+
+telescope.load_extension("fzf")
+telescope.load_extension("live_grep_args")
+telescope.load_extension("ui-select")
+telescope.load_extension("lazy")
+
+telescope.setup(opts)
+
+vim.keymap.set("n", "<leader>ff", builtin.find_files,               { desc = "Find files" })
+vim.keymap.set("n", "<leader>fg", builtin.live_grep,                { desc = "Live grep" })
+
+vim.keymap.set("n", "<leader>fb", builtin.buffers,                  { desc = "Find buffers" })
+vim.keymap.set("n", "<leader>fh", builtin.help_tags,                { desc = "Help tags" })
+
+vim.keymap.set("n", "<leader>fo", builtin.oldfiles,                 { desc = "Recent Files" })
+vim.keymap.set("n", "<leader>fk", builtin.keymaps,                  { desc = "Keymaps" })
+
+vim.keymap.set("n", "<leader>fs", builtin.lsp_document_symbols,     { desc = "Document symbols" })
+vim.keymap.set("n", "<leader>fS", builtin.lsp_workspace_symbols,    { desc = "Workspace symbols" })
+
+vim.keymap.set("n", "<leader>fc", builtin.commands,                 { desc = "命令列表" })
+
+vim.keymap.set("n", "<leader>fp", function()
+    require("telescope").extensions.lazy.lazy()
+end, { desc = "Lazy 插件列表" })
+
+vim.keymap.set("n", "<leader>gc", builtin.git_commits,              { desc = "Git commits" })
+vim.keymap.set("n", "<leader>gb", builtin.git_branches,             { desc = "Git branches" })
+vim.keymap.set("n", "<leader>gs", builtin.git_status,               { desc = "Git status" })
+
+
+-- vim.keymap.set("n", "<leader>fu", "<cmd>Telescope undo<cr>",        { desc = "Undo History" })
+
--- lua/plugins/ide.lua
+++ lua/plugins/ide.lua
@@ -1,6 +1,31 @@
 
 return {
     {
+        "nvim-telescope/telescope.nvim",
+        version = '*',                          -- 推荐锁定到最新发布版本，以避免潜在的兼容性问题
+        cmd = "Telescope",
+        keys = {
+            { "<leader>ff", desc = "查找文件" },
+            { "<leader>fg", desc = "全文搜索" },
+            { "<leader>fb", desc = "缓冲区" },
+            { "<leader>fh", desc = "帮助标签" },
+            { "<leader>fp", desc = "Lazy 插件列表" },
+        },
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons",
+
+            {"nvim-telescope/telescope-fzf-native.nvim", build = "make"},   -- 高性能FZF排序器
+            "nvim-telescope/telescope-live-grep-args.nvim",
+            "nvim-telescope/telescope-ui-select.nvim",                      -- 用 Telescope 接管 vim.ui.select 界面
+            "tsakirist/telescope-lazy.nvim",
+            -- "debugloop/telescope-undo.nvim",                    -- 撤销历史扩展
+        },
+        config = function()
+            require("configs.telescope")
+        end,
+    },
+    {
         "nvim-tree/nvim-tree.lua",
         dependencies = {
             "nvim-tree/nvim-web-devicons",
