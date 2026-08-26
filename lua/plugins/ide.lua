fcbdc214245b1b79c8d7730e76a16e117200cdfe
zhenquan.qiu <gnsyxiang@163.com>
2026-08-26 15:37:26 +0800

    feat: add noice.lua file

--- lua/plugins/ide.lua
+++ lua/plugins/ide.lua
@@ -47,5 +47,16 @@ return {
             require("configs.auto-session")
         end,
     },
+    {
+        "folke/noice.nvim",
+        event = "VeryLazy",
+        dependencies = {
+            "MunifTanjim/nui.nvim",
+            "rcarriga/nvim-notify",
+        },
+        config = function()
+            require("configs.noice")
+        end,
+    },
 }
 
