944a3c1790706be73885c18d7d8090a0ea012cbf
zhenquan.qiu <gnsyxiang@163.com>
2026-08-26 09:10:38 +0800

    feat: add wildfire.lua file

--- lua/plugins/encoding.lua
+++ lua/plugins/encoding.lua
@@ -11,6 +11,16 @@ return {
         config = function()
             require("configs.nvim-treesitter")
         end,
-    }
+    },
+    {
+        "sustech-data/wildfire.nvim",
+        event = "VeryLazy",                          -- 在 Neovim 启动完毕后加载
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter",         -- 必需依赖
+        },
+        config = function()
+            require("configs.wildfire")
+        end,
+    },
 }
 
