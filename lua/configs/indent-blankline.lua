13066dd50538c18f47be08442ba2555c3a6be63a
zhenquan.qiu <gnsyxiang@163.com>
2026-08-26 17:19:53 +0800

    fix: update indent-blankline.lua

--- lua/configs/indent-blankline.lua
+++ lua/configs/indent-blankline.lua
@@ -4,8 +4,8 @@ local opts = {
         char = "┆",                         -- 缩进竖线的字符（可选：│、┆、┊ 等）
     },
     scope = {
-        enabled = true,       -- 启用语法块范围高亮
-        show_start = true,    -- 显示代码块起始位置
+        enabled = false,       -- 启用语法块范围高亮
+        show_start = false,    -- 显示代码块起始位置
         show_end = false,     -- 不显示代码块结束位置（避免线条杂乱）
         highlight = { "Function" },        -- 高亮组
     },
