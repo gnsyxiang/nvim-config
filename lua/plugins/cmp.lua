1c66c3c7ffd0d8ed61e674c9a5a9dbeb2479e031
zhenquan.qiu <gnsyxiang@163.com>
2026-08-26 16:31:15 +0800

    feat: add snippets files

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,6 +1,21 @@
 
 return {
     {
+        "L3MON4D3/LuaSnip",
+        version = "v2.*",
+        build = "make install_jsregexp",
+        dependencies = { "rafamadriz/friendly-snippets" },
+        config = function()
+            require("luasnip.loaders.from_vscode").lazy_load({
+                -- paths = { vim.fn.stdpath("config") .. "/snippets" }
+                paths = { "./snippets" }
+            })
+
+            -- 加载友好片段
+            require("luasnip.loaders.from_vscode").lazy_load()
+        end,
+    },
+    {
         "hrsh7th/nvim-cmp",                     -- 补全引擎核心
         event = "InsertEnter",
         dependencies = {
