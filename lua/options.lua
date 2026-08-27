10fb01b51233deb703b54a9fa03ef6624d948231
zhenquan.qiu <gnsyxiang@163.com>
2026-08-27 10:00:46 +0800

    style: style for options.lua

--- lua/options.lua
+++ lua/options.lua
@@ -2,19 +2,19 @@
 local opt = vim.opt
 
 local options = {
-  clipboard = "unnamed,unnamedplus",      -- vim和系统剪贴板关联
+    clipboard = "unnamed,unnamedplus",      -- vim和系统剪贴板关联
 
-  wrap = false,                           -- 禁止自动换行
+    wrap = false,                           -- 禁止自动换行
 
-  -- 自动缩进
-  autoindent = true, 		                -- 继承上一行缩进
-  smartindent = true,                     -- 根据代码语法智能缩进
+    -- 自动缩进
+    autoindent = true, 		                -- 继承上一行缩进
+    smartindent = true,                     -- 根据代码语法智能缩进
 
-  -- 缩进相关配置
-  tabstop = 4,                            -- 每个Tab显示为4个空格宽度
-  shiftwidth = 4,                         -- 自动缩进和 >>/<< 操作的空格数
-  softtabstop = 4,                        -- 编辑时Tab键插入4个空格
-  expandtab = true,                       -- 将Tab转换为空格
+    -- 缩进相关配置
+    tabstop = 4,                            -- 每个Tab显示为4个空格宽度
+    shiftwidth = 4,                         -- 自动缩进和 >>/<< 操作的空格数
+    softtabstop = 4,                        -- 编辑时Tab键插入4个空格
+    expandtab = true,                       -- 将Tab转换为空格
 
     -- 搜索配置
     ignorecase = true,              -- 查找时忽略大小写
@@ -22,17 +22,17 @@ local options = {
     hlsearch = true,                -- 搜索高亮
     incsearch = true,               -- 边输入边搜索
 
-  number = true,                          -- 显示行号
-  relativenumber = true,                  -- 显示相对行号
-  cursorline = true,                      -- 高亮当前行
-  cursorcolumn = true,                    -- 高亮当前列
-  colorcolumn = "80",                     -- 右侧参考线
-  ruler = true,                           -- 显示光标位置
-  signcolumn = "yes",                     -- 始终显示符号列（避免窗口跳动）
+    number = true,                          -- 显示行号
+    relativenumber = true,                  -- 显示相对行号
+    cursorline = true,                      -- 高亮当前行
+    cursorcolumn = true,                    -- 高亮当前列
+    colorcolumn = "80",                     -- 右侧参考线
+    ruler = true,                           -- 显示光标位置
+    signcolumn = "yes",                     -- 始终显示符号列（避免窗口跳动）
 }
 
 vim.opt.shortmess:append 'c'
 for k, v in pairs(options) do
-  vim.opt[k] = v
+    vim.opt[k] = v
 end
 
