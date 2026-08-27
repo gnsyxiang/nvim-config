db8cce1f92a20b67aa83cf8dce50c81c51acb85e
zhenquan.qiu <gnsyxiang@163.com>
2026-08-27 11:22:33 +0800

    fix: update git.lua file

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -21,12 +21,20 @@ return {
         -- optional for floating window border decoration
         dependencies = {
             "nvim-lua/plenary.nvim",
+            "nvim-telescope/telescope.nvim",
         },
         -- setting the keybinding for LazyGit with 'keys' is recommended in
         -- order to load the plugin when the command is run for the first time
         keys = {
             { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
-        }
-    }
+        },
+        config = function()
+            require("telescope").load_extension("lazygit")
+
+            -- (可选) 配置 lazygit 浮动窗口的外观
+            -- vim.g.lazygit_floating_window_scaling_factor = 0.8
+            -- vim.g.lazygit_floating_window_border_chars = { '┌', '─', '┐', '│', '┘', '─', '└', '│' }
+        end,
+    },
 }
 
